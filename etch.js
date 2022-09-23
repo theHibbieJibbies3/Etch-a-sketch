22151813f2573f827b3e888d3516ccae670f14d9
theHibbieJibbies3 <113301369+theHibbieJibbies3@users.noreply.github.com>
2022-09-23 03:04:14 +0300

    basic div based drawing

--- etch.js
+++ etch.js
@@ -1,4 +1,4 @@
-pixAmount=16;
+pixAmount=30;
 let pixelize=90/pixAmount;
 
 
@@ -6,12 +6,12 @@ window.onload=function(){
     for(let i=0;i<pixAmount;i++){
         for(let c=0;c<pixAmount;c++){
         let div=document.createElement('div');
-        div.style.border='solid red';
-        div.style.height='10px';
         div.style.height=`${pixelize}vh`;
         div.style.width=`${pixelize}vh`;
         div.setAttribute('class','pixel');
+        div.addEventListener('mouseover', (event) => {div.style.backgroundColor='black'});
         document.getElementById("board").appendChild(div);
         }
     }
 }
+
